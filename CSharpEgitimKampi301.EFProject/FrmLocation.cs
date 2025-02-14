5daeded291e7e374897fe180d4ff5bb4930f5c8f
Yağmur K. <yagmurrkeserr1@gmail.com>
2025-02-15 00:13:47 +0300

    Lesson 16 / EF: Tur Projesi Location İşlemleri

---
+++ CSharpEgitimKampi301.EFProject/FrmLocation.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CSharpEgitimKampi301.EFProject
+{
+    public partial class FrmLocation : Form
+    {
+        public FrmLocation()
+        {
+            InitializeComponent();
+        }
+        EgitimKampiEfTravelDbEntities db = new EgitimKampiEfTravelDbEntities();
+        private void btnList_Click(object sender, EventArgs e)
+        {
+            var values = db.Location.ToList();
+            dataGridView1.DataSource = values;
+        }
+
+        private void FrmLocation_Load(object sender, EventArgs e)
+        {
+            var values = db.Guide.Select(x => new
+            {
+                FullName = x.GuideName + " " + x.GuideSurname,
+                x.GuideId
+            }).ToList();
+            cmbGuide.DisplayMember = "FullName";
+            cmbGuide.ValueMember = "GuideId";
+            cmbGuide.DataSource = values;
+        }
+
+        private void btnAdd_Click(object sender, EventArgs e)
+        {
+            Location location = new Location();
+            location.Capacity = byte.Parse(nudCapacity.Value.ToString());
+            location.City = txtCity.Text;
+            location.Country = txtCountry.Text;
+            location.Price = decimal.Parse(txtPrice.Text);
+            location.DayNight = txtDayNight.Text;
+            location.GuideId = int.Parse(cmbGuide.SelectedValue.ToString());
+            db.Location.Add(location);
+            db.SaveChanges();
+            MessageBox.Show("Ekleme işlemi başarıyla gerçekleşmiştir.");
+        }
+
+        private void btnDelete_Click(object sender, EventArgs e)
+        {
+            int id=int.Parse(txtId.Text);
+            var deleteValue=db.Location.Find(id);
+            db.Location.Remove(deleteValue);
+            db.SaveChanges();
+            MessageBox.Show("Silme işlemi başarıyla gerçekleşmiştir.");
+        }
+
+        private void btnUpdate_Click(object sender, EventArgs e)
+        {
+            int id = int.Parse(txtId.Text);
+            var updateValue = db.Location.Find(id);
+            updateValue.DayNight=txtDayNight.Text;
+            updateValue.Price = decimal.Parse(txtPrice.Text);
+            updateValue.Capacity=byte.Parse(nudCapacity.Value.ToString());
+            updateValue.City = txtCity.Text;
+            updateValue.GuideId = int.Parse(cmbGuide.SelectedValue.ToString());
+             var deleteValue=db.Location.Find(id);
+            db.SaveChanges();
+            MessageBox.Show("Güncelleme işlemi başarıyla gerçekleşmiştir.");
+        }
+    }
+}
