07b9f9e26e4d643e37dad0cf7df795e331999775
Yağmur K. <yagmurrkeserr1@gmail.com>
2025-03-02 12:17:02 +0300

    Lesson 18 / EntityState Komutları , Generic Repository Sınıfı ve EF Sınıfları

---
+++ CSharpEgitimKampi301.EntityLayer/Concrete/Order.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharpEgitimKampi301.EntityLayer.Concrete
+{
+    public class Order
+    {
+        public int OrderId { get; set; }
+        public int ProductId { get; set; }
+        public Product Product { get; set; }
+        public int Quantity { get; set; }
+        public decimal UnitPrice { get; set; }
+        public decimal TotalPrice { get; set; }
+        public int CustomerId { get; set; }
+        public Customer Customer { get; set; }
+
+    }
+}
